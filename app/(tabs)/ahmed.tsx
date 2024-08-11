2327cea289c82cbb4f87b356bb8f90e7b3e9598d
Ahmed <73728149+approxahmed@users.noreply.github.com>
2024-08-12 01:59:58 +0400

    feat: ahmed and jsonPage components

---
+++ app/(tabs)/ahmed.tsx
@@ -0,0 +1,53 @@
+import { StyleSheet, Text, TouchableHighlight, View } from "react-native";
+import JsonPage from "./jsonPage";
+
+export default function Ahmed() {
+  // TODO: replace the local server with a cloud infra, maybe render.com
+  const fetchDataFromServer: any = async () => {
+    try {
+      // Get data exposed from local server
+      const response = await fetch("http://localhost:3000/");
+
+      // imagine the content being the coordinates, with x and y coordinates
+      // obtained from the image to display the bounding boxes on the UI
+      const content = await response.json();
+
+      // TODO: add callback fn to render the data on the UI
+      <JsonPage jsonData={content} refreshData />;
+
+      console.log(content);
+    } catch (err) {
+      console.log("Error: Failed to get data from server");
+      console.error(err); // display the full error
+    }
+  };
+
+  return (
+    <View style={{ display: "flex" }}>
+      <View>
+        <Text style={styles.header}>Ahmed is testing</Text>
+      </View>
+      <View>
+        <TouchableHighlight onPress={() => fetchDataFromServer()}>
+          <Text style={styles.button}>Get data from server</Text>
+        </TouchableHighlight>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    // alignContent: "center",
+    // justifyContent: "center",
+    textAlign: "center",
+    fontSize: 32,
+    backgroundColor: "red",
+  },
+  button: {
+    backgroundColor: "lightblue",
+    padding: 5,
+    textAlign: "center",
+    fontSize: 24,
+  },
+});
