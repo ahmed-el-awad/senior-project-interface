71265614d6e309403e005925b1a5ca590e3d0aef
Ahmed <73728149+approxahmed@users.noreply.github.com>
2024-09-10 15:52:39 +0400

    feat: settings and dashboard pages

---
+++ app/(tabs)/dashboard.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function DashboardScreen() {
+  return (
+    <View style={{ backgroundColor: "blue" }}>
+      <Text style={{ color: "white", padding: 5 }}>Dashboard</Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function SettingsScreen() {
+  return (
+    <View style={{ backgroundColor: "green" }}>
+      <Text style={{ color: "white", padding: 5 }}>Settings</Text>
+    </View>
+  );
+}
