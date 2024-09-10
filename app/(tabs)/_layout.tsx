b1aeadbf0802a4df2e7dae148a4f03493d00081b
Ahmed <73728149+approxahmed@users.noreply.github.com>
2024-09-11 02:21:11 +0400

    refactor: move routes' order
    temporarily  moved index.tsx outside for now

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,26 +2,19 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
 import { Ionicons } from "@expo/vector-icons";
 
+// Layout of the sidebar
 export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer>
-        <Drawer.Screen
+        {/* <Drawer.Screen
           name="index"
           options={{
             drawerLabel: "Home",
             title: "Home",
             drawerIcon: () => <Ionicons name="home-outline" size={24} />,
           }}
-        />
-        <Drawer.Screen
-          name="settings"
-          options={{
-            drawerLabel: "Settings",
-            title: "Settings",
-            drawerIcon: () => <Ionicons name="settings-outline" size={24} />,
-          }}
-        />
+        /> */}
         <Drawer.Screen
           name="liveFeed"
           options={{
@@ -30,12 +23,21 @@ export default function Layout() {
             drawerIcon: () => <Ionicons name="videocam-outline" size={24} />,
           }}
         />
+
         <Drawer.Screen
           name="dashboard"
           options={{
             drawerLabel: "Dashboard",
             title: "Dashboard",
             drawerIcon: () => <Ionicons name="analytics-outline" size={24} />,
+          }}
+        />
+        <Drawer.Screen
+          name="settings"
+          options={{
+            drawerLabel: "Settings",
+            title: "Settings",
+            drawerIcon: () => <Ionicons name="settings-outline" size={24} />,
           }}
         />
       </Drawer>
