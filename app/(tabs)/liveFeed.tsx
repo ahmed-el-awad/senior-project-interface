8a8c0b542ed698264c92aa36b835374c2453f19f
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2024-09-24 10:00:02 +0400

    refactor: liveFeed.tsx style change

--- app/(tabs)/liveFeed.tsx
+++ app/(tabs)/liveFeed.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Video, ResizeMode } from "expo-av"; // Use expo-av for video
 import { MaterialIcons } from "@expo/vector-icons"; // Import icons from expo/vector-icons
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 const LiveFeed = () => {
   return (
@@ -12,22 +15,29 @@ const LiveFeed = () => {
         </Text>
         <Text style={styles.title}>Live Stream</Text>
       </View>
-      <Video
-        source={{
-          uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
-        }} // Use a direct video URL
-        style={styles.video}
-        resizeMode={ResizeMode.CONTAIN}
-        useNativeControls
-        shouldPlay
-      />
-      <Text style={styles.countText}>Count: 0</Text>
+      <View>
+        <View style={{ display: "flex", alignContent: "center", alignItems: "center" }}>
+          <Video
+            source={{
+              uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4",
+            }} // Use a direct video URL
+            style={styles.video}
+            resizeMode={ResizeMode.CONTAIN}
+            useNativeControls
+            shouldPlay
+          />
+        </View>
+        <Text style={styles.countText}>Count: 0</Text>
+        <Text style={styles.countText}>Habitat: Field</Text>
+        <Text style={styles.countText}>Elevation: </Text>
+      </View>
     </View> // Counter not working just displaying text for now
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
@@ -35,7 +45,7 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     flexDirection: "row", // Align children horizontally
-    alignItems: "center",
+    alignItems: "flex-start",
     marginBottom: 20,
   },
   title: {
@@ -45,9 +55,9 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   video: {
-    width: 480,
-    height: 270,
-    backgroundColor: "black",
+    width: width / 2,
+    height: height / 3,
+    // backgroundColor: "black",
   },
   countText: {
     marginTop: 20, // Add some space above the text
