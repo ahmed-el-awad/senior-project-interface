30b38efb5d2fa53b79addb2f9153a935f24c5e90
Ahmed <73728149+ahmed-el-awad@users.noreply.github.com>
2024-09-24 09:55:35 +0400

    feat: settings.tsx style change

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -2,7 +2,14 @@ import { Text, View } from "react-native";
 
 export default function SettingsScreen() {
   return (
-    <View style={{ backgroundColor: "green" }}>
+    <View
+      style={{
+        backgroundColor: "green",
+        display: "flex",
+        alignContent: "center",
+        // justifyContent: "center",
+      }}
+    >
       <Text style={{ color: "white", padding: 5 }}>Settings</Text>
     </View>
   );
